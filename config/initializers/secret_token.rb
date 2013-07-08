b20063de0d4d5329e206f394d3a7c972199347dd
Gabriel Brenner <gabejbrenner@gmail.com>
2013-07-07 23:49:43 -0500

    initialise rails server

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gameboard::Application.config.secret_token = '04490f26eb6dd759eac6d8f341c7ab252248ead78d60125ee957eaa7269010f6f156f18add91714a6c4c2a5fd0cda926d979cc133a031bf850311e0624d80715'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Gameboard::Application.config.session_store :cookie_store, key: '_gameboard_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Gameboard::Application.config.session_store :active_record_store
